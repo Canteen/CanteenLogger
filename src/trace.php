d960a45c93e1782e4e080fa2bda31a372ae318a0
mattmoore <matt@cloudkid.com>
2013-10-14 15:47:33 -0400

    Setup

---
+++ src/trace.php
@@ -0,0 +1,22 @@
+<?php
+	
+	/**
+	*  @module global
+	*/
+	use Canteen\Logger\Logger;
+	
+	/**
+	*  Convenience function for giving debug trace statements.  
+	*  __This is a global function.__
+	*  
+	*	trace('Point reached in code!');
+	*  
+	*  @class trace
+	*  @constructor
+	*  @param {mixed} object The object to trace
+	*  @param {String} [level=Logger::GENERAL] The log level
+	*/
+	function trace($object, $level=Logger::GENERAL)
+	{
+	    Logger::instance()->log($object, $level);
+	}
