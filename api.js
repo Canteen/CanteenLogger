0a01ecb5d414afe06ec29345c20da13c71ace2d0
mattmoore <matt@cloudkid.com>
2013-10-14 15:48:41 -0400

    Added docs

---
+++ api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Logger",
+        "debug",
+        "error",
+        "info",
+        "trace",
+        "warning"
+    ],
+    "modules": [
+        "Canteen_Logger",
+        "global"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen/Logger",
+            "name": "Canteen_Logger"
+        },
+        {
+            "displayName": "global",
+            "name": "global"
+        }
+    ]
+} };
+});
